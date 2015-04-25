b0fd6f46988784bd429287e7a57cac7a8ebdf818
CaseyKelly <casey.kelly@colorado.edu>
2015-04-25 14:24:47 -0600

    added one guitar as first product

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var app = angular.module('store', []);
+(function() {
+  var app = angular.module('store', [ ]);
+
+  app.controller('StoreController', function(){
+    this.guitar = guitar;
+  });
+
+  var guitar = {
+    make: 'Gibson',
+    model: 'Les Paul Custom',
+    year: 1973,
+    description: 'Cherry sunburst with rosewood inlay',
+    photoUrl: 'http://www.jimihocking.com/_images/_guitars/1973lespaul_sunburst.jpg'
+  };
+})();
